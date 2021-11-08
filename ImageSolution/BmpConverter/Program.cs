dacc7d44cc926ce0ba131456941e9d1ede5d9e8d
hyuckkim <monkeyhyuck@naver.com>
2021-11-08 13:57:33 +0900

    codes

---
+++ ImageSolution/BmpConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+foreach (FileInfo file in new DirectoryInfo("images").GetFiles())
+{
+    string filebody = file.FullName.Split('.').First();
+    Image img = Image.FromFile(file.FullName);
+
+    img.Save($"{filebody}.png", ImageFormat.Png);
+    img.Save($"{filebody}.jpeg", ImageFormat.Jpeg);
+    img.Save($"{filebody}.gif", ImageFormat.Gif);
+    img.Save($"{filebody}-0.bmp", ImageFormat.Bmp);
+}
